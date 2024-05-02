29525962d788285247174780eba196e753c6842d
Arokaap <aaron.david.nagore@gmail.com>
2024-05-02 23:54:16 +0200

    feat(ingress): Add LiveKit ingress integration with RTMP and WHIP

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home (): JSX.Element {
   return (
     <div className='flex flex-col gap-y-4'>
-      <h1>Señores está noche fiesta</h1>
+      <h1>Hola mama</h1>
 
     </div>
   )
